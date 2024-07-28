8657574636665ccc75edba538a7f50811ec9f025
Mohamed Hussain H <83116894+hussain033@users.noreply.github.com>
2024-07-28 18:23:22 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -6,15 +6,12 @@ llm = HuggingFaceEndpoint(
     task="text-generation",
     huggingfacehub_api_token="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",
     temperature = 0.6,
-    max_new_tokens = 200,
+    max_new_tokens = 100,
     return_full_text = False
 ) 
 from langchain_huggingface import ChatHuggingFace
 chat_llm = ChatHuggingFace(llm = llm)
-chain = chat_with_limit | StrOutputParser()
-
-def chat_with_limit(prompt):
-  return chat_llm.invoke(prompt, max_tokens = 300, stop = None)
+chain = chat_llm | StrOutputParser()
 
 def chat(question):
     return chain.invoke(question)
