4c78e0144d8cb72a4f4fb450df95b643b6578ff5
Mohamed Hussain H <83116894+hussain033@users.noreply.github.com>
2024-07-28 16:08:44 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -2,16 +2,19 @@ from langchain_huggingface import HuggingFaceEndpoint
 from langchain_core.output_parsers import StrOutputParser
 
 llm = HuggingFaceEndpoint(
-    repo_id="HuggingFaceH4/zephyr-7b-alpha",
+    repo_id="HuggingFaceH4/zephyr-7b-beta",
     task="text-generation",
     huggingfacehub_api_token="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",
     temperature = 0.6,
-    max_new_tokens = 80,
+    max_new_tokens = 200,
     return_full_text = False
 ) 
 from langchain_huggingface import ChatHuggingFace
 chat_llm = ChatHuggingFace(llm = llm)
-chain = chat_llm | StrOutputParser()
+chain = chat_with_limit | StrOutputParser()
+
+def chat_with_limit(prompt):
+  return chat_llm.invoke(prompt, max_tokens = 300, stop = None)
 
 def chat(question):
     return chain.invoke(question)
