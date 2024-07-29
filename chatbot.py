7160acd60b2e790d42446eb2685853148de2f7a9
Mohamed Hussain H <83116894+hussain033@users.noreply.github.com>
2024-07-29 11:44:23 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -4,6 +4,7 @@ from langchain_core.output_parsers import StrOutputParser
 llm = HuggingFaceEndpoint(
     repo_id="HuggingFaceH4/zephyr-7b-beta",
     task="text-generation",
+    huggingfacehub_api_token="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",
     temperature = 0.6,
     max_new_tokens = 100,
     return_full_text = False
